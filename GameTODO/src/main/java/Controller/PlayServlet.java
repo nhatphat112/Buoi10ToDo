b0adf115cfec205799ff7f441a9ec4d8743fc5e9
nhatphat112 <127173620+nhatphat112@users.noreply.github.com>
2023-04-20 22:31:03 +0700

    Update PlayServlet.java

--- GameTODO/src/main/java/Controller/PlayServlet.java
+++ GameTODO/src/main/java/Controller/PlayServlet.java
@@ -27,10 +27,12 @@ public class PlayServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/login.jsp").forward(req,resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         req.setCharacterEncoding("UTF-8");
         Users users = new Users();
         String url = "/play.jsp";
